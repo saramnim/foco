6bf410ec65dd43743cfad5c901dd0055e5c5a148
홍희선 <hia990618@gmail.com>
2022-12-19 01:34:10 +0900

    refector: UI에 관련없는 상위 컴포넌트 파일 옮김

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './component/Header/Header';
 import MainMap from './component/MainMap/MainMap';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { GlobalStyle } from './GlobalStyle';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <GlobalStyle />
-        <Header />
-        <Routes>
-          <Route path="/" element={<MainMap />} />
-        </Routes>
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainMap />} />
+      </Routes>
     </div>
   );
 }
