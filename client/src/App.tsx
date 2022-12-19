0e7ffbca2afc420e10d73dfd35244935910392fa
홍희선 <hia990618@gmail.com>
2022-12-19 22:28:05 +0900

    refector: UI에 관련없는 상위 컴포넌트 파일 옮김

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import './App.css';
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
-import { GlobalStyle } from './GlobalStyle';
+import { Routes, Route } from 'react-router-dom';
 import Header from './component/Header/Header';
 import MainMap from './component/MainMap/MainMap';
 import Register from './component/Register/Register';
@@ -10,25 +9,24 @@ import Profile from './component/Account/Profile';
 import Security from './component/Account/Security';
 import Deactivate from './component/Account/Deactivate';
 import Review from './component/Post/Post';
+import Detailmodal from './component/Detailmodal/Detailmodal';
+import Scrap from './component/Scrap/Scrap';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <GlobalStyle />
-        <Header />
-        <Routes>
-          <Route path="/" element={<MainMap />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/account/profile" element={<Profile />} />
-          <Route path="/account/security" element={<Security />} />
-          <Route path="account/deactivate" element={<Deactivate />} />
-          <Route path="/review" element={<Review />} />
-          <Route path="/scrap" element={<Scrap />} />
-          <Route path="/detailModal" element={<Detailmodal />} />
-        </Routes>
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainMap />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/account/profile" element={<Profile />} />
+        <Route path="/account/security" element={<Security />} />
+        <Route path="account/deactivate" element={<Deactivate />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/scrap" element={<Scrap />} />
+        <Route path="/detailModal" element={<Detailmodal />} />
+      </Routes>
     </div>
   );
 }
