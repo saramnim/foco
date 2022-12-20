ba1f41fd72a70b91bb4b3c0f2afcee736403971b
홍희선 <hia990618@gmail.com>
2022-12-20 15:54:07 +0900

    feat: header 메인페이지 이동 링크 추가

--- client/src/component/Header/Header.tsx
+++ client/src/component/Header/Header.tsx
@@ -2,11 +2,14 @@ import { HeaderWrapper, Title, Icons } from './style';
 import { HiUser } from 'react-icons/hi2';
 import { MdOutlineLanguage } from 'react-icons/md';
 import { ImSpoonKnife } from 'react-icons/im';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
     <HeaderWrapper>
-      <Title>FoCo</Title>
+      <Link to="/">
+        <Title>FoCo</Title>
+      </Link>
       <Icons>
         <MdOutlineLanguage />
         <HiUser />
