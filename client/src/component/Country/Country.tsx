c9d2b9b2a59c6b544a0840119a98ac5fe6745ad0
홍희선 <hia990618@gmail.com>
2022-12-21 14:32:33 +0900

    refector: selectBox 컴포넌트 분리

--- client/src/component/Country/Country.tsx
+++ client/src/component/Country/Country.tsx
@@ -3,7 +3,6 @@ import {
   ContentsWrapper,
   CountryWrapper,
   Title,
-  SelectBoxWrapper,
   City,
   HoverBottom,
   Content,
@@ -13,11 +12,11 @@ import {
   TotalLike,
 } from './style';
 import ImgShawdow from './imgshawdow.png';
+import MultiSelectBox from '../MultiSelectBox/MultiSelectBox';
 import { useParams } from 'react-router';
 import { HiHeart } from 'react-icons/hi';
 import { ImSpoonKnife } from 'react-icons/im';
 import { MdLocationOn } from 'react-icons/md';
-import Select from 'react-select';
 import axios from 'axios';
 
 interface Icontent {
@@ -28,11 +27,6 @@ interface Icontent {
   img: string;
   foodType: string[];
   mood: string[];
-}
-
-interface Iselect {
-  value: string;
-  label: string;
 }
 
 const Country = () => {
@@ -56,52 +50,10 @@ const Country = () => {
     fetchData();
   }, []);
 
-  // 서버 기능 연결하기 전 임시로 mock 데이터를 불러오기 위한 함수
-  // TODO : 서버 기능 완료시, 각 데이터마다 요청 url을 작성해야함
-  const cityType = () => {
-    const allCityTypes = data.map((content: Icontent) => {
-      return content.city;
-    });
-    return Array.from(new Set(allCityTypes.flat())).map((type: string) => {
-      return {
-        value: type,
-        label: type,
-      };
-    });
-  };
-
-  const foodType = () => {
-    const allFoodTypes = data.map((content: Icontent) => {
-      return content.foodType;
-    });
-    return Array.from(new Set(allFoodTypes.flat())).map((type: string) => {
-      return {
-        value: type,
-        label: type,
-      };
-    });
-  };
-
-  const moodType = () => {
-    const allMoodTypes = data.map((content: Icontent) => {
-      return content.mood;
-    });
-    return Array.from(new Set(allMoodTypes.flat())).map((type: string) => {
-      return {
-        value: type,
-        label: type,
-      };
-    });
-  };
-
   return (
     <CountryWrapper>
       <Title>{country}</Title>
-      <SelectBoxWrapper>
-        <Select isMulti={true} options={cityType()} className="selectBox" />
-        <Select isMulti={true} options={foodType()} className="selectBox" />
-        <Select isMulti={true} options={moodType()} className="selectBox" />
-      </SelectBoxWrapper>
+      <MultiSelectBox />
       <ContentsWrapper>
         {data
           .filter((content: Icontent) => {
