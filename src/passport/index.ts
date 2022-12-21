5ab9e4163e9b6083439e54a74f628a9f39a811cc
허재 <huhwo00@gmail.com>
2022-12-21 09:07:19 +0900

    구글로그인

--- src/passport/index.ts
+++ src/passport/index.ts
@@ -1,8 +1,10 @@
-import passport from "passport";
-import { local } from "./strategies/local";
-import { jwt } from "./strategies/jwt";
+import passport from 'passport';
+import { local } from './strategies/local';
+import { jwt } from './strategies/jwt';
+import { google } from './strategies/google';
 
 module.exports = () => {
   passport.use(local);
   passport.use(jwt);
+  passport.use(google);
 };
