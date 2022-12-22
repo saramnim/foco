d6b105e35d2f02f6c9347347153fa58d9cd3deb9
kailey <kailey.lee224@gmail.com>
2022-12-22 16:39:54 +0900

    design: 비밀번호 찾기 페이지, 헤더 로그인 메뉴 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Header from './component/Header/Header';
 import MainMap from './component/MainMap/MainMap';
 import Register from './component/Register/Register';
 import Login from './component/Login/Login';
+import ForgotPassword from './component/ForgotPassword/ForgotPassword';
 import Profile from './component/Account/Profile';
 import Security from './component/Account/Security';
 import Deactivate from './component/Account/Deactivate';
@@ -22,6 +23,7 @@ function App() {
         <Route path="/list/:country" element={<Country />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/account/profile" element={<Profile />} />
         <Route path="/account/security" element={<Security />} />
         <Route path="account/deactivate" element={<Deactivate />} />
