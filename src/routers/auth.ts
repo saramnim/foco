841939990dee99e89dbefeda5ec902e3a58ab883
허재 <huhwo00@gmail.com>
2022-12-22 10:54:24 +0900

    비밀번호변경하기 구현완료

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -6,6 +6,9 @@ import { hashPassword } from '../utils/hash-password';
 import { asyncHandler } from '../utils/async-handler';
 import { local } from '../passport/strategies/local';
 import { google } from '../passport/strategies/google';
+import { generateRandomPassword } from '../utils/generate-random-password';
+import { sendMail } from '../utils/send-mail';
+import { loginRequired } from '../middlewares/login-required';
 
 const authRouter = Router();
 
@@ -41,6 +44,10 @@ authRouter.post(
   }),
   asyncHandler((req, res, next) => {
     setUserToken(res, req.user);
+    if (req.user.passwordReset) {
+      res.redirect('/auth/change-password');
+      return;
+    }
     res.redirect('/');
   })
 );
@@ -81,6 +88,67 @@ authRouter.delete(
     const shortId = req.params.shortId;
     const deletedUser = await User.findOneAndDelete({ shortId });
     res.json(deletedUser);
+  })
+);
+
+//비밀번호찾기 페이지 요청 핸들러
+authRouter.get('/reset-password', (req, res, next) => {
+  res.render('reset-password');
+});
+
+//새비밀번호 받기 요청 핸들러
+authRouter.post(
+  '/reset-password',
+  asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error('해당 메일로 가입된 사용자가 없습니다.');
+    }
+    const PW = generateRandomPassword();
+    await User.findOneAndUpdate(
+      { email },
+      {
+        password: hashPassword(PW),
+        passwordReset: true,
+      }
+    );
+    sendMail(
+      email,
+      '비밀번호가 변경되었습니다.',
+      `변경된 비밀번호는: ${PW}입니다.`
+    );
+    res.render('reset-password-confirmed');
+  })
+);
+
+//비밀번호 변경하기 페이지
+authRouter.get('/change-password', (req, res, next) => {
+  res.render('change-password');
+});
+
+//비밀번호 변경 요청 핸들러
+authRouter.post(
+  '/change-password',
+  asyncHandler(async (req, res) => {
+    const { current_password, new_password } = req.body;
+    const user = await User.findOne({ shortId: req.user.shortId });
+    if (!user) {
+      throw new Error('유저가 없습니다.');
+    }
+    if (user.password !== hashPassword(current_password)) {
+      throw new Error('임시 비밀번호가 일치하지 않습니다.');
+    }
+
+    await User.updateOne(
+      { shortId: user.shortId },
+      {
+        password: hashPassword(new_password),
+        passwordReset: false,
+      }
+    );
+
+    res.redirect('/logout');
   })
 );
 
