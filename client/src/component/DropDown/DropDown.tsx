f2b0ec59944f35579c1da4925029b28d871f9db3
홍희선 <hia990618@gmail.com>
2022-12-28 18:10:29 +0900

    feat: bookMark 나라별 필터링 기능 추가

--- client/src/component/DropDown/DropDown.tsx
+++ client/src/component/DropDown/DropDown.tsx
@@ -2,15 +2,27 @@ import { useState, useEffect } from 'react';
 import { DropDownWrapper } from './style';
 import axios from 'axios';
 import Select from 'react-select';
+import { Icontent } from './../Icontent';
 
 const DropDown = (props: any) => {
   const [countries, setCountries] = useState<any[]>([]);
   const [path, setPath] = useState<string[]>([]);
+  const [country, setCountry] = useState<string>('');
+  const userNum = localStorage.getItem('userNum');
+
+  console.log(countries);
   const options = countries.map((x) => {
-    return {
-      value: x.properties.name,
-      label: x.properties.name,
-    };
+    if (props.title != 'My BookMark') {
+      return {
+        value: x.properties.name,
+        label: x.properties.name,
+      };
+    } else {
+      return {
+        value: x,
+        label: x,
+      };
+    }
   });
 
   const findCordinates = (country: string) => {
@@ -33,6 +45,7 @@ const DropDown = (props: any) => {
       url: 'http://localhost:4000/Data/worldmap.json',
     }).then((res) => {
       setCountries(res.data.objects.world.geometries);
+      console.log(res.data.objects.world.geometries); //배열임
     });
   };
 
@@ -45,9 +58,24 @@ const DropDown = (props: any) => {
     });
   };
 
+  const getBookMarkCountries = () => {
+    return axios({
+      method: 'get',
+      url: `/bookmark/${userNum}`,
+    }).then((res) => {
+      const bookMarkCountries = res.data.map((x: any) => {
+        return x.country;
+      });
+      const uniqueArray = Array.from(new Set(bookMarkCountries));
+      setCountries(uniqueArray);
+    });
+  };
+
   useEffect(() => {
     const fetchData = async () => {
-      await getCountriesName();
+      props.title != 'My BookMark'
+        ? await getCountriesName()
+        : await getBookMarkCountries();
     };
     fetchData();
   }, []);
@@ -59,6 +87,7 @@ const DropDown = (props: any) => {
 
   const handleCountry = (selected: any) => {
     props.setCountry(selected.value);
+    setCountry(selected.value);
   };
 
   return (
